7549ad2a4aadf46ee80dc14ad3846d9e18b28950
virgil.si <virgil.si@fjdynamics.com>
2022-12-07 16:18:54 +0800

    cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -11,4 +11,22 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
+    build: {
+        // outDir: "lib-name", // 默认 dist
+        lib: { //库编译模式配置
+            entry: path.resolve(__dirname, './packages/index.ts'), // 指定编译入口文件
+            name: 'AutoAxios',
+            fileName: (format) => `build.${format}.ts`
+        },
+        rollupOptions: { // rollup打包配置
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: ['vue'],
+            output: {
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    }
 })
